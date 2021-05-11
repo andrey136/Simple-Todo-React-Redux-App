6610a9657009f292f6aa70df700ebdfe8e8fd244
andrey_mardash <andrey.mardash@gmail.com>
2021-05-11 06:41:01 +0300

    Modified todoSlice.js

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,16 +1,61 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from '@reduxjs/toolkit';
 
 export const slice = createSlice({
     name: 'todo',
     initialState: {
-        todoList: ["Go to the movies", "Do homework"]
+        todoList: [
+            {
+                todo: "Go to the movies",
+                done: false,
+                id: nanoid()
+            },
+            {
+                todo: "Do homework",
+                done: false,
+                id: nanoid()
+            }
+        ]
     },
     reducers: {
-        addToDo: (state, todoItemAction) => {
-            state.todoList = [...state.todoList, todoItemAction.payload]
+        // addToDo: (state, todoItemAction) => {
+        //     state.todoList = [...state.todoList, todoItemAction.payload]
+        // },
+        addToDo: {
+            reducer(state, action){
+                console.log(state, 'Action State')
+                state.todoList.push(action.payload)
+            },
+            prepare(todo){
+                return {
+                    payload: {
+                        id: nanoid(),
+                        todo,
+                        done: false
+                    }
+                }
+            }
         }
     }
 })
+
+// postAdded: {
+//     reducer(state, action){
+//         state.push(action.payload);
+//     },
+//     prepare(title, content, userId){
+//         return {
+//             payload: {
+//                 id: nanoid(),
+//                 title,
+//                 date: new Date().toISOString() ,
+//                 content,
+//                 user: userId,
+//             }
+            
+//         }
+//     }
+// },
 
 export const { addToDo } = slice.actions
 
