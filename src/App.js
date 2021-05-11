9ac4599865f8234b8a3b5f24df5635bf6de520e9
andrey_mardash <andrey.mardash@gmail.com>
2021-05-11 06:36:27 +0300

    Added TodoList form/list in tha main App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import {Todo} from './features/todo/Todo';
+import {TodoList} from './features/todo/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <TodoList />
     </div>
   );
 }
