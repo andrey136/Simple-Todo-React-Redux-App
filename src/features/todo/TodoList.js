3338ff0f5447fac3265a22102ccd3edf6fe8e60b
andrey_mardash <andrey.mardash@gmail.com>
2021-05-16 21:28:02 +0300

    Changed the state in src/features/todo/TodoList.js

---
+++ src/features/todo/TodoList.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToDo, todoSelect } from './todoSlice';
+
+export function TodoList(){
+    // const todoList = useSelector(todoSelect());
+    const todoList = useSelector(todoSelect);
+
+    const [inputText, setIntutText] = useState('');
+    const dispatch = useDispatch();
+
+    return(
+        <div>
+           <h1>ToDo List</h1>
+        <input onChange={(e) => setIntutText(e.target.value)} value={inputText}></input>
+        <button className="btn btn-primary" onClick={() => {dispatch(addToDo(inputText)); setIntutText('')}}>Add ToDo</button>
+        <ul>
+            { todoList.map( (el, key) => 
+            <li key={key}>{el.todo}</li>
+            ) }
+        </ul>
+        </div> 
+    );
+}
